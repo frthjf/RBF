b9252bbb23f51978414cfe504eed0a0c93668229
Frithjof Gressmann <fg14@illinois.edu>
2022-08-12 20:43:24 -0500

    Specify version in pyproject.toml instead of extracting it from version control information

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-if __name__ == '__main__':
+if __name__ == "__main__":
     from setuptools import setup
     from setuptools.extension import Extension
     from Cython.Build import cythonize
@@ -18,29 +18,35 @@ if __name__ == '__main__':
         )
 
     cy_ext = []
-    cy_ext += [Extension(name='rbf.poly', sources=['rbf/poly.pyx'])]
-    cy_ext += [Extension(name='rbf.sputils', sources=['rbf/sputils.pyx'])]
-    cy_ext += [Extension(name='rbf.pde.halton', sources=['rbf/pde/halton.pyx'])]
-    cy_ext += [Extension(name='rbf.pde.geometry', sources=['rbf/pde/geometry.pyx'])]
-    cy_ext += [Extension(name='rbf.pde.sampling', sources=['rbf/pde/sampling.pyx'])]
+    cy_ext += [Extension(name="rbf.poly", sources=["rbf/poly.pyx"])]
+    cy_ext += [Extension(name="rbf.sputils", sources=["rbf/sputils.pyx"])]
+    cy_ext += [Extension(name="rbf.pde.halton", sources=["rbf/pde/halton.pyx"])]
+    cy_ext += [Extension(name="rbf.pde.geometry", sources=["rbf/pde/geometry.pyx"])]
+    cy_ext += [Extension(name="rbf.pde.sampling", sources=["rbf/pde/sampling.pyx"])]
     ext = cythonize(cy_ext)
 
-    with open('rbf/_rbf_ufuncs/metadata.json', 'r') as f:
+    with open("rbf/_rbf_ufuncs/metadata.json", "r") as f:
         rbf_ufunc_metadata = json.load(f)
 
     for itm in rbf_ufunc_metadata:
-        ext += [Extension(name=itm['module'], sources=itm['sources'], include_dirs=[np.get_include()])]
+        ext += [
+            Extension(
+                name=itm["module"],
+                sources=itm["sources"],
+                include_dirs=[np.get_include()],
+            )
+        ]
 
     setup(
-        name='RBF',
-        version=version_info['__version__'],
-        description='Package containing the tools necessary for radial basis '
-                    'function (RBF) applications',
-        author='Trever Hines',
-        author_email='treverhines@gmail.com',
-        url='www.github.com/treverhines/RBF',
-        packages=['rbf', 'rbf.pde'],
+        name="RBF",
+        version="1.0",
+        description="Package containing the tools necessary for radial basis "
+        "function (RBF) applications",
+        author="Trever Hines",
+        author_email="treverhines@gmail.com",
+        url="www.github.com/treverhines/RBF",
+        packages=["rbf", "rbf.pde"],
         ext_modules=ext,
         include_package_data=True,
-        license='MIT'
-        )
+        license="MIT",
+    )
